edbae461567c9993446817d21e44f1db1f27380c
Qubaka <kozikuba204@gmail.com>
2023-04-12 11:58:32 +0200

    added footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import BubbleContainer from './components/BubbleContainer/BubbleContainer';
 import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
       <BubbleContainer></BubbleContainer>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import './Footer.css';
+
+function Footer() {
+
+  return (
+    <div className='Footer'>
+        <div className='Marker'>
+            © HydroSpark Team
+        </div>
+    </div>
+  );
+}
+
+export default Footer;
