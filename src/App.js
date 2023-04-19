e70492d83b4bdadbcab503e4391525b4edfdecc9
Nadisi <Nadia@examp.com>
2023-04-19 10:15:29 +0200

    playing with page layout, to unfix position footer an bubbles

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import './App.css';
-import Navbar from './components/Navbar/Navbar';
-import Footer from './components/Footer/Footer';
 import MainPage from './components/MainPage/MainPage';
 
 function App() {
   return (
     <div className="App">
-      <Navbar login={false}></Navbar>
       <MainPage></MainPage>
-      <Footer></Footer>
     </div>
   );
 }
--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -2,13 +2,17 @@ import BubbleContainer from '../BubbleContainer/BubbleContainer';
 import LoginMenu from '../LoginMenu/LoginMenu';
 import RegisterMenu from '../RegisterMenu/RegisterMenu';
 import './MainPage.css';
+import Navbar from '../Navbar/Navbar';
+import Footer from '../Footer/Footer';
 
 function MainPage() {
   return (
     <div className='MainPage'>
+      <Navbar login={false}></Navbar>
       <LoginMenu></LoginMenu>
       <RegisterMenu></RegisterMenu>
       <BubbleContainer color={"rgba(255, 255, 255, 0.2)"}></BubbleContainer>
+      <Footer></Footer>
     </div>
   );
 }
