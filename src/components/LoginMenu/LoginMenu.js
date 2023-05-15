e5d65969090d16cdb883b5e57bd51e83b5c7a6be
unknown <200417pepe@gmail.com>
2023-05-15 10:23:05 +0200

    add axios coonecton with backend

--- src/components/LoginMenu/LoginMenu.js
+++ src/components/LoginMenu/LoginMenu.js
@@ -1,18 +1,44 @@
-
+import React, { useState } from 'react';
+import axios from 'axios';
 import './LoginMenu.css';
 
 function LoginMenu() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('http://localhost:3000/login', { email, password });
+      console.log(response.data); // handle the response as needed
+    } catch (error) {
+      console.error('Failed to log in:', error);
+    }
+    setEmail("");
+    setPassword("");
+  };
+  
   return (
     <div id='LoginMenu'>
       <p className="loginTitle">LOG IN</p>
-      <form className='loginForm'>
+      <form className='loginForm' onSubmit={handleSubmit}>
         <div>
           <label>Email</label>
-          <input placeholder='Email'></input>
+          <input
+            type='email'
+            placeholder='Email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
           <label>Password</label>
-          <input placeholder='Password'></input>
+          <input
+            type='password'
+            placeholder='Password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
-        <button id='LoginButtonForm'>Log in</button>
+        <button id='LoginButtonForm' type='submit'>Log in</button>
       </form>
     </div>
   );
--- src/components/RegisterMenu/RegisterMenu.js
+++ src/components/RegisterMenu/RegisterMenu.js
@@ -1,20 +1,51 @@
-
+import React, { useState } from 'react';
+import axios from 'axios';
 import './RegisterMenu.css';
+import { useNavigate } from "react-router-dom";
 
 function RegisterMenu() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [reppassword, repsetPassword] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      navigate("/");
+      const response = await axios.post('http://localhost:3000/register', { email, password,reppassword });
+      console.log(response); // handle the response as needed
+      
+    } catch (error) {
+      console.error('Failed to log in:', error);
+    }
+  };
+
   return (
     <div id='RegisterMenu'>
       <p className="registerTitle">REGISTER</p>
-      <form className='registerForm'>
+      <form className='registerForm' onSubmit={handleSubmit}>
         <div>
           <label>Email</label>
-          <input placeholder='Email'></input>
+          <input type='email'
+            placeholder='Email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}></input>
           <label>Password</label>
-          <input placeholder='Password'></input>
+          <input type='password'
+            placeholder='Password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          ></input>
           <label>Repeat Password</label>
-          <input placeholder='Password'></input>
+          <input type='password'
+            placeholder='Password'
+            value={reppassword}
+            onChange={(e) => repsetPassword(e.target.value)}
+          ></input>
         </div>
-        <button id='RegisterButtonForm'>Register</button>
+        <button id='RegisterButtonForm' type='submit'
+        >Register</button>
       </form>
     </div>
   );
