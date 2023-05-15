65befbee578f61aa7260b44e872f66dc4d2cf208
Piotr Kwasniowski <kwasniowskipiotr5@gmail.com>
2023-05-15 23:16:51 +0200

    add map for devices and write data from all devices which user has conected to his account

--- src/components/Device/Device.js
+++ src/components/Device/Device.js
@@ -1,17 +1,19 @@
 import './Device.css';
 
-function Device() {
+function Device(props) {
+  console.log(props.device)
   return (
     <div className='Device'>
         <div>
             <div className='devbox1'>
-                <p>Device Name</p>
+                <p>{props.device.name}</p>
                 <button>More</button>
             </div>
             <div className='devbox2'>
-                <p>ID: 56rtyd45erty67rtyu</p>
+                <p>ID: {props.device._id}</p>
                 <p>Info</p>
-                <p>Some more info</p>
+                <p>power: {props.device.power}</p>
+                <p>power daily: {props.device.powerDaily}</p>
             </div>
         </div>
     </div>
--- src/components/UserPage/UserPage.js
+++ src/components/UserPage/UserPage.js
@@ -7,11 +7,14 @@ import { useEffect, useState } from 'react';
 function UserPage() {
   const [id, setID] = useState("64621715de8e4fab450c6ad8"); // Store the user ID obtained from the login process (currently hardcoded for demonstration)
   const [email, setEmail] = useState("");
+  const [devices,setDevices] = useState([]);
   useEffect(() => {
     const fetchUserData = async () => {
       try {
         const response = await axios.get(`http://localhost:3000/getUserData/${id}`);
+        const response2 = await axios.get(`http://localhost:3000/devices/${id}/getDevices`);
         setEmail(response.data.email);
+        setDevices(response2.data);
       } catch (error) {
         console.error('Failed to fetch user data:', error);
       }
@@ -28,13 +31,10 @@ function UserPage() {
   return (
     <div id='UserPage'>
       <div className='UserTitle'>Welcome {email}</div>
-      <Device></Device>
-      <Device></Device>
-      <Device></Device>
-      <Device></Device>
-      <Device></Device>
-      <Device></Device>
-      <Device></Device>
+      {devices.map(element => (
+        <Device device={element} key={element.id} />
+      ))}
+      
     </div>
   );
 }
