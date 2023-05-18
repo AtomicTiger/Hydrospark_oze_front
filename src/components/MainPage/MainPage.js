26950d2ae27c7b03661fb99bb6d78d70cd051f0b
Kukalooka <kukalkapol@gmail.com>
2023-05-18 08:35:19 +0200

    Added some more stuff

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -9,12 +9,7 @@ function MainPage() {
       </div>
       <div className='Aside'>
         <h1>Witaj w Hydrospark</h1>
-        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor i
-ncididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
-exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
- dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
-mollit anim id est laborum.</p>
+        <p>Hydrospark to innowacyjne rozwiązanie, która umożliwia wytwarzanie energii elektrycznej z wody za pomocą turbiny wodnej w domowym zaciszu. Dzięki tej aplikacji użytkownicy mogą zainstalować turbinę wodną w pobliżu źródła wody, monitorować produkcję energii, kontrolować zużycie oraz oszczędzać na rachunkach za energię elektryczną, wykorzystując potencjał wody jako czystego źródła energii odnawialnej. To innowacyjne rozwiązanie przyczynia się do zrównoważonej i ekologicznej przyszłości, umożliwiając jednostkom produkowanie i kontrolowanie własnej energii elektrycznej.</p>
         <div className="buttonCont">
           <img src={require('./ios.png')} alt="Download on Appstore Button"></img>
           <img src={require('./gplay.png')} alt="Download on Google Play Button"></img>
