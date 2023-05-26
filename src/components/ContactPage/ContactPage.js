63c503a0cbb3e005968be3e93f4c873f17f8362d
Kukalooka <kukalkapol@gmail.com>
2023-05-26 20:56:20 +0200

    Added map

--- src/components/ContactPage/ContactPage.js
+++ src/components/ContactPage/ContactPage.js
@@ -1,9 +1,32 @@
 import './ContactPage.css';
+import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from 'react-leaflet';
+import { useState, useEffect } from 'react';
+import 'leaflet/dist/leaflet.css';
 
-function ContactPage() {
+function ContactPage(props) {
+  const [clickedLatLng, setClickedLatLng] = useState({lat: 50.203360237954996, lng: 19.206428186368925});
+  
+  const MyComponent = () => {
+    return null;
+  }
+
   return (
     <div className='ContactPage'>
-        Page z kontaktami = potrzebny tekst i grafiki
+        <div className='Aside'>
+          <h1>Kontakt</h1>
+          <p>Adres: Promienna 65, 43-603 Jaworzno</p>
+        </div>
+        <MapContainer center={[50.203360237954996, 19.206428186368925]} zoom={16} className="map">
+          <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+          <MyComponent />
+          {clickedLatLng && (
+            <Marker position={clickedLatLng}>
+              <Popup>
+                CKZIU Jaworzno {clickedLatLng.lat.toFixed(4)}, {clickedLatLng.lng.toFixed(4)}
+              </Popup>
+            </Marker>
+          )}
+        </MapContainer>
     </div>
   );
 }
