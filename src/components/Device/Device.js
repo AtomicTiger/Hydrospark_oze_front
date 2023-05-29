f74cf0f0a5fe4fe75ead363f0f08f344f77f4067
Qubaka <kozikuba204@gmail.com>
2023-05-29 09:02:42 +0200

    Almost working device name change

--- src/components/Device/Device.js
+++ src/components/Device/Device.js
@@ -1,9 +1,37 @@
 import './Device.css';
+import { useState } from 'react';
+import axios from 'axios';
 
 function Device(props) {
   //console.log(props.device[props.index])
-  const changeName = (e) =>{
-    console.log("whhhoooooooowwwies you want o change ethe name huh?");
+  const [NewName, setNewName] = useState(props.device.name);
+  const id = props.device._id;
+  const [ ButtonAction, setButtonAction] = useState(false);//false if input is not visible, true if visible and changing name
+  const InputAction =(event)=>{
+    setNewName(event.target.value);
+  }
+
+
+  const changeName = async (event) =>{
+    if(!ButtonAction){
+      console.log("whhhoooooooowwwies you want o change ethe name huh?");
+      //console.log(event.target.previousSibling) //input
+      //console.log(event.target.previousSibling.previousSibling) //title
+      event.target.previousSibling.classList.remove('hidden');
+      event.target.previousSibling.previousSibling.classList.add('hidden');
+      setButtonAction(true)
+    }else{
+      event.target.previousSibling.classList.add('hidden');
+      event.target.previousSibling.previousSibling.classList.remove('hidden');
+      setButtonAction(false)
+      //here zapytanie do bazy danych o zmianę imienia 
+      try{
+        const response = await axios.post('https://hydrospar.onrender.com/changename', { id, NewName });
+        console.log(response);
+        }catch{
+        console.error('shit blow up')
+      }
+    }
 
   }
 
@@ -11,7 +39,8 @@ function Device(props) {
     <div className='Device'>
         <div>
             <div className='devbox1'>
-                <p>{props.device.name}</p>
+                <p className='DeviceTitle'>{props.device.name}</p>
+                <input className='DeviceInput hidden' type='text' onChange={InputAction}></input>
                 <button onClick={changeName}>Change name</button>
             </div>
             <div className='devbox2'>
